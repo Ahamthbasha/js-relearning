6e2016fdfa9d7e1648f04a74ac152f233ba89bfb
ahamathbasha <bashachristopherallah786@gmail.com>
2025-02-05 19:37:28 +0530

    daily js questions

---
+++ dailyOnequestion.js
@@ -0,0 +1,24 @@
+// Write a JavaScript program to display the current day and time in the following format.
+// Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+// const date=new Date()
+// const Day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+// let hoursAmPm=date.getHours()
+// if(hoursAmPm > 12 && hoursAmPm <=24){
+//     hoursAmPm=date.getHours()-12+" PM"
+// }else{
+//     hoursAmPm=date.getHours()-12+"AM"
+// }
+// console.log("Today is:",Day[date.getDay()])
+// console.log(`CurrentTime is:${hoursAmPm}:${date.getMinutes()} minutes:${date.getSeconds()} seconds`)
+
+
+//2.print current Window contents
+
+// function content(){
+//      in dev tools you use window.print()
+//      console.log(global)
+// }
+
+// content()
