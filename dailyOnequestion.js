a82eb31de5e246c12a4525523b5923511002ff6a
ahamathbasha <bashachristopherallah786@gmail.com>
2025-03-15 18:59:08 +0530

    closure

--- dailyOnequestion.js
+++ dailyOnequestion.js
@@ -22,3 +22,18 @@
 // }
 
 // content()
+
+function greet(){
+    let a=20
+    let b=30
+
+    function meet(){
+        console.log(a)
+        console.log(b)
+    }
+
+    return meet
+}
+
+const num=greet()
+num()
